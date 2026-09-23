c8698387bbf694984b3fc21c15ca573eb6a9e360
Nicolas Perez <nikkoperez@gmail.com>
2026-09-23 15:52:25 -0500

    -Added faker dependencies -Added tsconfig file -Changed variable names in the .env file -Updated the loginTest file -Added CsvtoJsonUtil file -Added FakerDataUtil file

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,46 +1,41 @@
 // Test script using the Page Object Model
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import { encrypt } from "../utils/CryptojsUtil";
 import { decrypt } from "../utils/CryptojsUtil";
 import { decryptEnvFile, encryptEnvFile } from "../utils/EncryptEnvFile";
 import logger from "../utils/LoggerUtil";
 
-const authFile = "src/config/auth.json"
+const authFile = "src/config/auth.json";
 
-test("Login and create the storage state json file", async({page}) => {
+test("Login and create the storage state json file", async ({ page }) => {
+  const loginPage = new LoginPage(page);
 
-    const loginPage = new LoginPage(page);
-
-    await loginPage.navigateToLoginPage();
-    await loginPage.fillUsername(decrypt(process.env.userid!))
-    await loginPage.clickLoginButton();
-    await loginPage.fillPassword(decrypt(process.env.password!))
-    await loginPage.clickLoginButton();
-    // const homePage = await loginPage.clickLoginButton();
-    // await homePage.expectServiceTitleToBeVisible();
-    logger.info("Test for login is completed");
-    await page.context().storageState({ path: authFile});
+  await loginPage.navigateToLoginPage();
+  await loginPage.fillUsername(decrypt(process.env.SALESFORCE_USER!));
+  await loginPage.clickLoginButton();
+  await loginPage.fillPassword(decrypt(process.env.SALESFORCE_PASSWORD!));
+  await loginPage.clickLoginButton();
+  // const homePage = await loginPage.clickLoginButton();
+  // await homePage.expectServiceTitleToBeVisible();
+  logger.info("Test for login is completed");
+  await page.context().storageState({ path: authFile });
 });
 
-test("Login with auth file", async({browser}) => {
-
-    const context = await browser.newContext({storageState: authFile});
-    const page = await context.newPage();
-    await page.goto(
-        "https://orgfarm-18068347c3-dev-ed.develop.lightning.force.com/lightning/page/home"
-    );
-    await expect(page.getByRole("link", {name: "Accounts"})).toBeVisible();
+test("Login with auth file", async ({ browser }) => {
+  const context = await browser.newContext({ storageState: authFile });
+  const page = await context.newPage();
+  await page.goto("https://orgfarm-18068347c3-dev-ed.develop.lightning.force.com/lightning/page/home");
+  await expect(page.getByRole("link", { name: "Accounts" })).toBeVisible();
 });
 
-test('Sample env test', async({page}) => {
-
-    // const plaintext = 'Hello, Nico';
-    // const encryptedText = encrypt(plaintext);
-    // console.log('SALT:', process.env.SALT);
-    // console.log('Encrypted:', encryptedText);
-    // const decryptedText = decrypt(encryptedText);
-    // console.log('Decrypted:', decryptedText);
-    encryptEnvFile();
-    // decryptEnvFile();
+test("Sample env test", async ({ page }) => {
+  // const plaintext = 'Hello, Nico';
+  // const encryptedText = encrypt(plaintext);
+  // console.log('SALT:', process.env.SALT);
+  // console.log('Encrypted:', encryptedText);
+  // const decryptedText = decrypt(encryptedText);
+  // console.log('Decrypted:', decryptedText);
+  encryptEnvFile();
+  //   decryptEnvFile();
 });
